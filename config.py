77a43ba2423b593bef16a6295dc2dbbaf744d288
zhulispace <295042027@qq.com>
2019-12-25 15:32:19 +0800

    delete

--- config.py
+++
@@ -1,8 +0,0 @@
-class config(object):
-    device='cuda'
-    img_dir = r'/home/luoyc/zhulin/img_crop'
-    dict_path=r'/home/luoyc/zhulin/textDR/utils/dict.txt'
-    num_txt_path=r'/home/luoyc/zhulin/textDR/utils/num_train.txt'
-    text_txt_path=r'/home/luoyc/zhulin/textDR/utils/text_train.txt'
-    img_rgb=False
-opt=config()
